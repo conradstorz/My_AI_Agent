77d443440e1503b030b84ad2df07a272c72f6b3a
Conrad Storz <conradstorz@gmail.com>
2025-05-30 11:57:32 -0400

    attempt to log exceptions

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from pathlib import Path
 from loguru import logger
 from agent import agent_loop
 from dotenv import dotenv_values, load_dotenv
+import sys
 
 load_dotenv() 
 
 from utils.startup_checks import run_startup_diagnostics
-
 
 # Where to store agent heartbeat info
 HEARTBEAT_FILE = Path("heartbeat/agent.status.json")
@@ -33,6 +33,14 @@ def main():
     LOGS_DIR = Path("logs")
     LOGS_DIR.mkdir(exist_ok=True)
     logger.add(LOGS_DIR / "main.log", rotation="1 week")
+
+    # This is an attempt to log any uncaught exceptions suggested by ChatGPT
+    # https://stackoverflow.com/questions/18070767/logging-uncaught-exceptions-in-python
+    def log_uncaught_exceptions(exctype, value, tb):
+        # exc_info will include (type, value, traceback)
+        logger.error("Uncaught exception!", exc_info=(exctype, value, tb))
+
+    sys.excepthook = log_uncaught_exceptions    
 
     logger.info("Checking imports...")
     run_startup_diagnostics()
